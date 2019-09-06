8c9c81ac89e2823b7f7ece1bbd38d2ff32612814
sms0406 <39329737+sms0406@users.noreply.github.com>
2019-09-06 17:56:56 +0530

    Update capstore_admin.sql

--- capstore_admin.sql
+++ capstore_admin.sql
@@ -1,6 +1,10 @@
 create database capStore;
 use capStore;
 create table merchant(merchant_Id bigint primary key auto_increment,merchant_Name  varchar(40) not null, merchant_Type varchar(15) not null, phone_No varchar(10) not null, email varchar(50) not null,password varchar(15),govt_Proof_Type  varchar(10),govt_Proof varchar(16));
+insert into merchant values(1001,"Raju Enterprises","Direct","1234567890","raju@gmail.com","raju1234","Adhaar","1234567898745632");
+insert into merchant values(1002,"Raju Enterprises","Direct","1234567890","raju@gmail.com","raju1234","Adhaar","1234567898745632");
+insert into merchant values(1003,"Raju Enterprises","Direct","1234567890","raju@gmail.com","raju1234","Adhaar","1234567898745632");
+
 create table adminlogin(admin_Id int primary key auto_increment,name varchar(20),email varchar(30),password varchar(30),mobno varchar(20));
 insert into adminlogin values(1,'mayank','mayank@gmail.com','1234','9803538343');
 drop table adminlogin;
