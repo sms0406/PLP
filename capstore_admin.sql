f55eafecac9bf30dbc173ccb9de7a2c05362c4b9
sms0406 <39329737+sms0406@users.noreply.github.com>
2019-09-07 09:20:47 +0530

    Update capstore_admin.sql

--- capstore_admin.sql
+++ capstore_admin.sql
@@ -9,3 +9,22 @@ create table adminlogin(admin_Id int primary key auto_increment,name varchar(20)
 insert into adminlogin values(1,'mayank','mayank@gmail.com','1234','9803538343');
 drop table adminlogin;
 select * from adminlogin;
+
+CREATE TABLE customer(
+  customer_id int(25) PRIMARY KEY auto_increment,
+  name varchar(50) Not NULL,
+  email varchar(50) NOT NULL,
+  password varchar(20) NOT NULL,
+  address varchar(50) NOT NULL,
+  mobile_no varchar(12) NOT NULL,
+  gender varchar(10) Not NULL
+);
+
+CREATE TABLE customer_orders (
+  
+  customer_id int NOT NULL references customer(customer_id),
+  product_name varchar(20) NOT NULL,
+  order_id  int primary key auto_increment,
+  quantity int,
+  amount double(10,2)
+) ;
